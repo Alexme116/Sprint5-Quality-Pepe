c21e4dd6f8fb5ee5d9a9602968ec97e9d9c60498
Alexme116 <alexmoncada16@hotmail.com>
2025-06-11 22:37:05 -0600

    Playwright first Base tests

--- end-to-end/tests/tasks.spec.ts
+++ end-to-end/tests/tasks.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('Login Loaded', async ({ page }) => {
-  await page.goto('http://localhost:8080');
+  await page.goto('http://localhost:3000');
 
-  const title = page.getByText('Oracules Manager');
+  const title = page.getByText('Sign in to MyTodoList');
   await expect(title).toBeVisible();
+
+  const emailInput = page.getByPlaceholder('Enter your email address');
+  await expect(emailInput).toBeVisible();
 });
